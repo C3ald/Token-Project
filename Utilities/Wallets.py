aa2e98c497d75314e309a9d17beaeb898c16bc5c
C3ald <specklesjr10@gmail.com>
2022-05-02 20:32:57 -0700

    the start of onion signing

--- Utilities/Wallets.py
+++ Utilities/Wallets.py
@@ -1,10 +1,20 @@
 from Cryptodome.PublicKey import ECC
 from Cryptodome.Signature import DSS
 from Cryptodome.Hash import SHA512
+from Cryptodome.Random import *
+from Cryptodome.Cipher import AES
 import uuid
 import string
 import random
 from binascii import unhexlify
+import hashlib
+import codecs
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.fernet import Fernet
 
 
 class Wallet_generation:
@@ -56,17 +66,76 @@ class Onion_Signatures:
 	def __init__(self):
 		pass
 
-	def make_signature(self, signature, blockchain):
+	def make_signature(self, transaction):
 		""" picks a random number of signatures to add to the current one """
 		# Miner generates a random number of keys to use for encryption and the sender and the receiver encrypt their own sets of the miner's keys
 		# Miner's randomly generated keys encrypt the sender and receiver
 		# add keys used for encryption to a new part called encoding and combine the sender's publickey and the key for decrypting with it and do the same with receiver in a different set
 		# make it look like this: {'sender_encoding': sender+decryptionkey, 'receiver_encding': receiver+decryptionkey}
+		sender = transaction['sender']
+		receiver = transaction['receiver']
+		amount = transaction['amount']
+		signature_of_sender = transaction['signature']
 
 
-	def convert_signature_to_list(self, signature):
-		list_sign = list(signature)
-		return list_sign
+
+	# def combine(self, publickey, decryption_key):
+	# 	""" Combines the sender and receiver with the decryption key, hashes the sender and receiver then combines it with the decryption key """
+	# 	encrypted_key = hashlib.sha256(publickey.encode()).hexdigest()
+	# 	encrypted_key = bytes(encrypted_key.encode())
+	# 	encrypted_key = int.from_bytes(encrypted_key, 'big')
+	# 	encrypted_decryption = bytes(decryption_key.encode())
+	# 	encrypted_decryption = int.from_bytes(encrypted_decryption, 'big')
+	# 	print(encrypted_decryption)
+	# 	algorithm = encrypted_key * encrypted_decryption
+	# 	print(algorithm)
+	# 	result = hex(algorithm)
+	# 	return result
+
+
+	# def decrypt(self, publickey, encrypted_data):
+	# 	encrypted_key = hashlib.sha256(publickey.encode()).hexdigest()
+	# 	encrypted_key = bytes(encrypted_key.encode())
+	# 	encrypted_public = int.from_bytes(encrypted_key, 'big')
+	# 	encrypted_data = bytes.fromhex(encrypted_data[2:])
+	# 	encrypted_data = int.from_bytes(encrypted_data, 'big')
+	# 	algorithm = encrypted_data / encrypted_public
+	# 	print(algorithm)
+	# 	to_hex = hex(int(algorithm))[2:]
+	# 	#print(to_hex)
+	# 	to_bytes = bytes.fromhex(to_hex)
+	# 	#to_bytes = unhexlify(to_int)
+	# 	return to_bytes.decode()
+
+	def decrypt(self, publickey:str, encrypted_data:str):
+		encrypted_pub = hashlib.sha256(publickey.encode()).hexdigest()
+		decryption_key = encrypted_data.replace(encrypted_pub, '')
+		return decryption_key
+
+
+	def combine(self, publickey, encryption_key):
+		encrypted_pub = hashlib.sha256(publickey.encode()).hexdigest()
+		random_place = random.randint(0, len(encrypt_key))
+		new_string = encryption_key[:random_place] + encrypted_pub + encryption_key[random_place:]
+		return new_string
+
+
+
+	def encrypt(self, data:str, key=None):
+		encoded = data.encode()
+		if key == None:
+			key = self.generate_new_key()
+		cipher = AES.new(key, AES.MODE_EAX)
+		encrypted = cipher.encrypt_and_digest(data)
+		return encrypted
+		
+
+
+	def generate_new_key(self):
+		key = get_random_bytes(32)
+		cipher = AES.new(key, AES.MODE_EAX)
+		cipher = cipher.hexdigest()
+		return cipher
 
 
 
@@ -79,6 +148,14 @@ if __name__ == '__main__':
 	print(priv_key)
 	signer = Signatures()
 	signature = signer.sign(priv_key, 'Alice')
-	print(signature)
+	#print(signature)
 	valid = signer.verify(pub_key, 'Alice', signature)
-	print(valid)
+	#print(valid)
+	onion = Onion_Signatures()
+	encrypt_key = onion.generate_new_key()
+	print(encrypt_key)
+	key = onion.combine('Alice', encrypt_key)
+	print(key)
+	decrypt = onion.decrypt('Alice', key)
+	
+	print(decrypt)
